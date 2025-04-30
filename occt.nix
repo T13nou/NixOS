a1a6e9a6e873a90aa0c4456107d14a1ee496fc16
T13nou <64090484+T13nou@users.noreply.github.com>
2025-04-30 16:43:43 +0200

    Update occt.nix

--- occt.nix
+++ occt.nix
@@ -1,32 +1,32 @@
-{ stdenv, fetchurl, autoPatchelfHook
-, gtk3, libX11, libXext, libXmu, libXi, libGL, libGLU
-}:
+with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
-  pname = "occt";
-  version = "14.0.8";
+  pname    = "occt";
+  version  = "14.0.9";
 
   src = fetchurl {
-    url = "https://www.ocbase.com/download/edition:Personal/os:Linux";
-    # SHA256 du binaire OCCT 14.0.8 (à calculer et insérer ici)
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    url    = "https://www.ocbase.com/download/edition:Personal/os:Linux";
+    # Remplacez par le hash réel retourné par `nix-prefetch-url`
+    sha256 = "sha256-i221nPvigSZAeu7i5Y2HVyBoaZQsHPIR0qhKaQhknwQ=";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook ];
+  # On ne veut plus des phases unpack et build
+  phases = [ "installPhase" ];
 
-  # Pas de phase de compilation : on installe directement le binaire
+  nativeBuildInputs = [ autoPatchelfHook ];
+  buildInputs       = [ gtk3 xorg.libX11 xorg.libXext xorg.libXmu xorg.libXi lib
+GL libGLU icu ];
+
   installPhase = ''
     mkdir -p $out/bin
-    install -m755 OCCT $out/bin/OCCT
+    install -Dm755 $src $out/bin/OCCT
   '';
 
-  buildInputs = [ gtk3 libX11 libXext libXmu libXi libGL libGLU ];
-
-  meta = with stdenv.lib; {
-    description = "OCCT (OverClock Checking Tool) – outil de test de stabilité et de monitoring matériel";
+  meta = {
+    description = "OCCT ${version} – test de stabilité et bench PC (Personal Edi
+tion)";
     homepage    = "https://www.ocbase.com/";
-    license     = licenses.unfree;
-    platforms   = platforms.linux;
-    # (licence : logiciel gratuit, mais non libre)
+    license     = lib.licenses.unfree;
+    platforms   = lib.platforms.linux;
   };
 }
